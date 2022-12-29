eb14e19ae76471501ab1c792f9d6e695b8deab07
LakshayKhurana <lakshay.1khurana@gmail.com>
2022-12-29 18:49:04 +0530

    updated file structure

--- data_dump.py
+++ data_dump.py
@@ -4,13 +4,13 @@ import json
 
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-file_path = '/config/workspace/aps_failure_training_set1.csv'
-database_name = 'aps'
-collection_name = 'sensor'
+FILE_PATH = '/config/workspace/aps_failure_training_set1.csv'
+DATABASE_NAME = 'aps'
+COLLECTION_NAME = 'sensor'
 
 if __name__ == '__main__':
-    df = pd.read_csv(file_path)
-    print('Rows & Columns of dataset:',df.shape)
+    df = pd.read_csv(FILE_PATH)
+    # print('Rows & Columns of dataset:',df.shape)
 
 # Convert dataframe to json in order to dump it into MongoDB
 
@@ -20,4 +20,4 @@ json_format = list(json.loads(df.T.to_json()).values())
 # print(json_format[0])
 
 # Insert converted json records to MongoDB
-client[database_name][collection_name].insert_many(json_format)
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_format)
